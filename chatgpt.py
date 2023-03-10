b7dcb1d97fdd9bed632f63c7205328f279bebb61
ke6nk <ke6nkh@gmail.com>
2023-03-10 14:06:35 +0330

    ver 1.0

---
+++ chatgpt.py
@@ -0,0 +1,17 @@
+import openai
+openai.api_key = "YOUR_API_KEY"
+question = None
+while(question != "end"):
+    print("\n")
+    question = input("Ask: ")
+    if question != "end":
+        res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"{question}"}
+        ]
+        )
+
+        response = res.choices[0].message["content"]
+
+        print(f"{response}\n")
